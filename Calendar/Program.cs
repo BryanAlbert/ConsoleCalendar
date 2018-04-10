2445f5f93fae4cdff373d33f96bcc49bced2268c
Bryan Albert <bryanalbert@hotmail.com>
2018-04-09 17:44:44 -0700

    Show today in Red.

--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -2,7 +2,8 @@
 
 /*
  * TODO: 
- * Add year parameter, defaulting to current year
+ *	 Use Red for today's date, DarkGray for prefix and suffix dates (before the 1st and after the last day)
+ * Add year parameter, defaulting to current year? 
  * Select the nearest month, i.e. if the curren tmonth is November 2017 and 1 is specified on the command line, show January 2018. 
  * First try to parse arguments as a DateTime (assemble a string as necessary), failing that, assume month, or month and year, as discussed.
  * Add command line switches:
@@ -10,7 +11,6 @@
  *		-diff subtract two DateTimes, output in year, month, day
  *		-julian julian date 
  *		-add add two DateTimes
- *		-color Use Yellow for today's date, shadow color for prefix and suffix dates (before the 1st and after the last day)
  */
 
 namespace Calendar
@@ -23,17 +23,24 @@ namespace Calendar
 			string title = month.ToString("MMMM yyyy");
 			Console.WriteLine("{0}{1}", new string(' ', (21 - title.Length) / 2), title);
 			Console.WriteLine("Su Mo Tu We Th Fr Sa");
+			ConsoleColor initialColor = Console.ForegroundColor;
 			for (int day = 1 - (int) new DateTime(month.Year, month.Month, 1).DayOfWeek; day <= DateTime.DaysInMonth(month.Year, month.Month);)
 			{
-				string week = "";
 				for (int weekDay = 0; weekDay < 7 && day <= DateTime.DaysInMonth(month.Year, month.Month); weekDay++, day++)
-					week += day > 0 ? string.Format("{0,2} ", day) : new string(' ', 3);
-				Console.WriteLine(week);
+				{
+					Console.ForegroundColor = DateTime.Now.Year == month.Year && DateTime.Now.Month == month.Month && DateTime.Now.Day == day ?
+						ConsoleColor.Red : ConsoleColor.White;
+					Console.Write(day > 0 ? string.Format("{0,2} ", day) : new string(' ', 3));
+				}
+				Console.WriteLine();
 			}
+
+			Console.ForegroundColor = initialColor;
 		}
 
 		private static DateTime ParseCommandLine(string[] args)
 		{
+			// parse as a DateTime, failing that, as an int
 			DateTime month;
 			if (args.Length == 0)
 				return DateTime.Now;
